efaf8bec2eb0274abb7204304451d64cb8be1fa7
CaveSpider <18310139362@163.com>
2021-06-22 11:44:35 +0800

    Field of depth implemented. #v4c

--- include/camera.hpp
+++ include/camera.hpp
@@ -5,7 +5,7 @@
 #include <vecmath.h>
 #include <float.h>
 #include <cmath>
-
+#include <cstdlib>
 
 class Camera {
 
@@ -48,21 +48,38 @@ private:
  
     float angle;
     float dis;
+    float focus;    // 焦平面距离
+    float field;    // 光圈大小
 
 public:
     PerspectiveCamera(const Vector3f &center, const Vector3f &direction,
-            const Vector3f &up, int imgW, int imgH, float _angle) : Camera(center, direction, up, imgW, imgH) {
+        const Vector3f &up, int imgW, int imgH, float _angle, float _focus = 0.0f , float _field = 0.0f)
+        :Camera(center, direction, up, imgW, imgH){
         // angle is in radian.
-        this->angle = _angle;
-        dis = (this->width / 2.0f) / tan(this->angle / 2.0f);
+        angle = _angle;
+        focus = _focus;
+        if(_focus == 0.0f) focus = dis;
+        field = _field;
+        dis = (width / 2.0f) / tan(angle / 2.0f);
+    }
+
+    Vector3f randDelta(){
+        // return a random delta based on field
+        float theta = 2 * M_PI * rand() / RAND_MAX;
+        float r = float(rand()) / RAND_MAX;
+        float rs = sqrt(r);
+        float dir = 1.0f;
+        if(float(rand()) / RAND_MAX < 0.5f) dir = -1.0f;
+        return (rs*cos(theta)*Vector3f::UP + rs*sin(theta)*Vector3f::RIGHT
+            + dir*sqrt(1-r)*Vector3f::FORWARD) * field;
     }
 
     Ray generateRay(const Vector2f &point) override {
-        // 产生一个intensity为1的光线
-        Vector3f intersect = this->center + direction * this->dis;  // center in screen
-        intersect += (point[0] - this->width / 2.0f) * this->horizontal;
-        intersect += (point[1] - this->height / 2.0f) * this->up;
-        return Ray(this->center, (intersect - this->center).normalized(), Vector3f(1, 1, 1), 0, true);
+        Vector3f intersect = center + direction * focus;  // center in screen
+        intersect += (point[0] - width / 2.0f) * (focus / dis) * horizontal;
+        intersect += (point[1] - height / 2.0f) * (focus / dis) * up;
+        Vector3f start = center + randDelta();
+        return Ray(start, (intersect - start).normalized(), Vector3f(1, 1, 1), 0, true);
     }
 };
 
